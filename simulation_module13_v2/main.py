b0f4996f6f0483a12c8d9f36017268cf6f1114d1
mauripsale <maurizio.ipsale@gmail.com>
2026-06-10 10:06:42 +0200

    cleanup: remove simulation artifacts from repository

--- simulation_module13_v2/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from simulation-module13-v2!")
-
-
-if __name__ == "__main__":
-    main()
--- simulation_module21_7_v2/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from simulation-module21-7-v2!")
-
-
-if __name__ == "__main__":
-    main()
